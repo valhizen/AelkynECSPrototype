e654da2fa859685513c679d32bd4dc3fb407c2a5
valhizen <valhizen@localhost.localdomain>
2026-03-24 14:23:57 +0545

    Adding Application

--- src/engine/app.rs
+++ src/engine/app.rs
@@ -1,1 +1,29 @@
+use winit::application::ApplicationHandler;
 
+struct App {}
+
+impl App {
+    pub fn new() -> Self {
+        Self {}
+    }
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &winit::event_loop::ActiveEventLoop) {}
+
+    fn window_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        window_id: winit::window::WindowId,
+        event: winit::event::WindowEvent,
+    ) {
+    }
+
+    fn device_event(
+        &mut self,
+        event_loop: &winit::event_loop::ActiveEventLoop,
+        device_id: winit::event::DeviceId,
+        event: winit::event::DeviceEvent,
+    ) {
+    }
+}
